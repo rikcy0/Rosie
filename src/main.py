70c066963bef9d6f31f0a45876bfd67a9728e3c2
Ricky Wen <rwen0880@gmail.com>
2024-01-20 16:20:53 -0800

    edited main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from runCommand import runCommand
+import random
 
 client = commands.Bot(command_prefix = '$', intents = discord.Intents.all())
 
@@ -11,5 +12,16 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello chatters")
+
+@client.command()
+async def ping(ctx):
+    bot_latency = round(client.latency * 1000)
+    await ctx.send(f"Ping: {bot_latency} ms.")
+
+@client.command(aliases=['motivate'])
+async def spread_positivity(ctx):
+    quote = ['**You look great today!**', '**Something great will happen today.**', '**Play league of legends!**',
+                '**You are awesome!**', '**Thanks for being you!**', '**You have a nice smile.**', '**Bombastic, you are so fantastic.**']
+    await ctx.send(random.choice(quote))
 
 runCommand(client)
